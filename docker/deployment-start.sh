af60be514653ca5160b7bfcaaf53bfd179c04d68
MISBAHULLL <misbahulmunir36843@gmail.com>
2026-08-09 10:15:48 +0700

    normalize Apache MPM at container startup

--- docker/deployment-start.sh
+++ docker/deployment-start.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
+rm -f \
+    /etc/apache2/mods-enabled/mpm_event.conf \
+    /etc/apache2/mods-enabled/mpm_event.load \
+    /etc/apache2/mods-enabled/mpm_worker.conf \
+    /etc/apache2/mods-enabled/mpm_worker.load
+
+a2enmod mpm_prefork >/dev/null
+apache2ctl configtest
+
 php artisan optimize
 
 exec apache2-foreground
